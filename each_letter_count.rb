6e29d65fb05e201a52324939ba4603978f108d70
Jelani Woods <jelani@firstdraft.com>
2022-12-02 11:54:08 -0600

    Update each_letter_count.rb

--- each_letter_count.rb
+++ each_letter_count.rb
@@ -2,5 +2,6 @@
 # 
 # Asks the user to enter a word.
 #  The program should print each letter in the word the number of times it appears in the word.
+# Look in the github README file for example output
 
 p "Enter a word:"
