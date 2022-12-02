8f5da24a2f5180cb8866e32da1527fea02d25136
Jelani Woods <jelani@firstdraft.com>
2022-12-02 11:54:27 -0600

    Update each_even_word.rb

--- each_even_word.rb
+++ each_even_word.rb
@@ -1,6 +1,6 @@
 # Write a program that:
 #   Asks the user to enter a list of words separated by spaces.
 #   The program should only print the words that are have an even number of characters
-# 
+# Look in the github README file for example output
 
 p "Enter a list of words separated by spaces:"
