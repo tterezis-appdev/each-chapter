de6d175e24bdefbf15fbf0ca53c5e993be874a5a
Teddy Terezis <tterezis@chicagobooth.edu>
2023-04-03 23:18:17 +0000

    Saved Work

--- each_even_word.rb
+++ each_even_word.rb
@@ -3,4 +3,16 @@
 #   The program should only print the words that are have an even number of characters
 # Look in the github README file for example output
 
-p "Enter a list of words separated by spaces:"
+puts "Enter a list of words separated by spaces:"
+array=gets.chomp.split
+array.each do |the_word|
+  if the_word.length%2==0
+    p the_word
+  end
+  # if |the_word|.length
+    # p the_word
+end
+
+
+
+  
